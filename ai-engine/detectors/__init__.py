92c919e4949a8a462fd6c5abcd691fec208641b5
tejuavi27913 <tejuavi27913@gmail.com>
2026-06-05 14:58:23 +0530

    Create __init__.py

---
+++ ai-engine/detectors/__init__.py
@@ -0,0 +1,17 @@
+from .person_detector import PersonDetector
+from .intrusion_detector import IntrusionDetector
+from .crowd_detector import CrowdDetector
+from .fire_detector import FireDetector
+from .smoke_detector import SmokeDetector
+from .face_detector import FaceDetector
+from .suspicious_detector import SuspiciousDetector
+
+__all__ = [
+    'PersonDetector',
+    'IntrusionDetector',
+    'CrowdDetector',
+    'FireDetector',
+    'SmokeDetector',
+    'FaceDetector',
+    'SuspiciousDetector'
+]
